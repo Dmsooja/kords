1308e1eeb5c19af3bc6c85abb27a6e37f9553dd7
Dmsooja <dev@sooja.fr>
2022-08-05 18:41:53 +0200

    adding tailwindcss

---
+++ .slicemachine/assets/slices/HeroSectionCard/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/HeroSectionCard';
+
+export default {
+  title: 'slices/HeroSectionCard'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Grain","spans":[]}],"description":[{"type":"paragraph","text":"In irure dolor sunt nulla duis minim adipisicing aliquip. Consequat laborum fugiat consectetur consectetur sint nulla laborum do.","spans":[]}]},"slice_type":"hero_section_card","id":"_Default"}} />
+_Default.storyName = ''
