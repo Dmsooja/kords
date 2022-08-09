ab8cdbf3daa5cc37f6d0add3d3baacf26e896b8c
Dmsooja <dev@sooja.fr>
2022-08-09 19:40:55 +0200

    update slices

---
+++ .slicemachine/assets/slices/ScrollingCards/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/ScrollingCards';
+
+export default {
+  title: 'slices/ScrollingCards'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Minerals","spans":[]}],"description":[{"type":"paragraph","text":"Veniam laboris Lorem ad non magna exercitation mollit dolor in. Laborum est Lorem incididunt proident adipisicing et consectetur proident eu occaecat ea irure voluptate culpa enim. Labore deserunt Lorem minim et nisi irure minim incididunt commodo deserunt culpa amet.","spans":[]}]},"slice_type":"scrolling_cards","id":"_Default"}} />
+_Default.storyName = ''
---
+++ .slicemachine/assets/slices/Text/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Text';
+
+export default {
+  title: 'slices/Text'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Empty","spans":[]}],"description":[{"type":"paragraph","text":"Lorem velit sint eu sit adipisicing veniam quis ullamco nulla. Exercitation quis culpa voluptate nulla ullamco laboris.","spans":[]}]},"slice_type":"text","id":"_Default"}} />
+_Default.storyName = ''
