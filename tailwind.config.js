45777c9d793c0d1a676815adb9fd959af1567da3
Dmsooja <dev@sooja.fr>
2022-08-09 19:35:52 +0200

    fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./slices/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primarygreen': '#CED5CF',
+        'primaryorange': '#FBA366',
+        'darkgreen': '#6E7E5F',
+        'black': '#000000',
+      },
+    },
   },
   plugins: [],
 }
