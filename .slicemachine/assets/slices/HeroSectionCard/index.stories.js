79f6bd0cd2482cb1308fa9aac2aa2148c123c4ff
Dmsooja <dev@sooja.fr>
2022-08-09 19:37:16 +0200

    Create hero slice for homepage

--- .slicemachine/assets/slices/HeroSectionCard/index.stories.js
+++ .slicemachine/assets/slices/HeroSectionCard/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Grain","spans":[]}],"description":[{"type":"paragraph","text":"In irure dolor sunt nulla duis minim adipisicing aliquip. Consequat laborum fugiat consectetur consectetur sint nulla laborum do.","spans":[]}]},"slice_type":"hero_section_card","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{"button_type":"primary","button_link":{"link_type":"Web","url":"https://prismic.io"},"button_text":[{"type":"paragraph","text":"Occaecat ea nulla minim nostrud incididunt. Aute pariatur Lorem velit do minim nulla nostrud consectetur consectetur sint irure Lorem.","spans":[]}]}],"primary":{"title":[{"type":"heading1","text":"Advice","spans":[]}],"description":[{"type":"paragraph","text":"Est ex proident qui consequat deserunt laboris ullamco tempor. Ipsum eiusmod consequat ut aute aliquip eu ut adipisicing proident. Culpa adipisicing ipsum veniam.","spans":[]}]},"slice_type":"hero_section_card","id":"_Default"}} />
 _Default.storyName = ''
