3734c0784cb89c2b6205960748796c93134fb6dd
Dmsooja <dev@sooja.fr>
2022-08-17 12:14:59 +0200

    Create an empty template for new slices

---
+++ .prismic/slice-template/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PrismicRichText, PrismicLink } from '@prismicio/react';
+
+
+
+const {{componentName}} = ({ slice }) => (
+  <section>
+  </section>
+)
+
+export default {{componentName}}
