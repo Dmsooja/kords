aef4623c295ffe903dc8bb9bfc575e9e15f15b9b
Dmsooja <dev@sooja.fr>
2022-08-25 12:54:35 +0200

    add a description to Demo Heading slice

--- .slicemachine/assets/slices/DemoSimpleHeading/index.stories.js
+++ .slicemachine/assets/slices/DemoSimpleHeading/index.stories.js
@@ -5,8 +5,8 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading3","text":"Fifth","spans":[]}]},"slice_type":"demo_simple_heading","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading3","text":"Instrument","spans":[]}]},"slice_type":"demo_simple_heading","id":"_Default"}} />
 _Default.storyName = ''
 
-export const _WithDescriptionAndCta = () => <MyComponent slice={{"variation":"withDescriptionAndCta","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading3","text":"Careful","spans":[]}],"description":[{"type":"paragraph","text":"Tempor Lorem qui adipisicing consectetur quis cupidatat eu aliqua incididunt voluptate pariatur culpa et. Consequat fugiat commodo velit fugiat. Lorem veniam proident consectetur laboris voluptate velit consectetur minim est magna irure culpa id.","spans":[]}],"button_link":{"link_type":"Web","url":"https://prismic.io"},"button_text":[{"type":"paragraph","text":"Reprehenderit reprehenderit tempor adipisicing minim ad tempor voluptate exercitation velit anim eiusmod amet.","spans":[]}]},"slice_type":"demo_simple_heading","id":"_WithDescriptionAndCta"}} />
-_WithDescriptionAndCta.storyName = ''
+export const _Withdescription = () => <MyComponent slice={{"variation":"withdescription","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading3","text":"Except","spans":[]}],"description":[{"type":"paragraph","text":"Id laborum officia laboris.","spans":[]}]},"slice_type":"demo_simple_heading","id":"_Withdescription"}} />
+_Withdescription.storyName = ''
--- slices/DemoSimpleHeading/index.js
+++ slices/DemoSimpleHeading/index.js
@@ -1,23 +1,11 @@
 import React from 'react'
-import { PrismicRichText, PrismicLink } from '@prismicio/react'
+import { PrismicRichText } from '@prismicio/react'
 
 const DemoSimpleHeading = ({ slice }) => (
   <section>
-    <div className="pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between">
-      <div>
-        <div className="text-lg leading-6 font-medium text-gray-900"><PrismicRichText field={slice.primary.title} /></div>
-        <div className="text-gray-500 text-sm">
-          <PrismicRichText field={slice.primary.description} />
-        </div>
-      </div>
-      <button
-        type='button'
-        className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-      >
-        <PrismicLink field={slice.primary.button_link}>
-          <PrismicRichText field={slice.primary.button_text} />
-        </PrismicLink>
-      </button>
+    <div className="pb-5 border-b border-gray-200">
+      <div className="text-lg leading-6 font-medium text-gray-900"><PrismicRichText field={slice.primary.title} /></div>
+      <div className='text-sm text-gray-500'><PrismicRichText field={slice.primary.description} /></div>
     </div>
   </section>
 )
