61fe2c35b817ae35490e4d914ebd52e00557757c
Dmsooja <dev@sooja.fr>
2022-08-30 16:06:24 +0200

    Add a description to demo heading

--- slices/NouhaHeading/index.js
+++ slices/NouhaHeading/index.js
@@ -4,15 +4,15 @@ import { PrismicRichText, PrismicLink } from '@prismicio/react';
 
 
 const NouhaHeading = ({ slice }) => (
-  <section className='my-4 max-w-7xl mx-auto sm:px-6 lg:px-8" pb-5 border-b border-gray-200'>
-    <div className='text-lg leading-6 font-medium text-gray-900'>
+  <div className="my-4 sm:flex sm:items-center sm:justify-between max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div className="pb-5 border-b border-gray-200">    <div className='text-lg leading-6 font-medium text-gray-900'>
       <PrismicRichText field={slice.primary.title} />
-      <p className="mt-2 max-w-4xl text-sm text-gray-500">
-        Workcation is a property rental website. Etiam ullamcorper massa viverra consequat, consectetur id nulla tempus.
-        Fringilla egestas justo massa purus sagittis malesuada.
-      </p>
+      <div className="mt-2 max-w-4xl text-sm text-gray-500">
+        <PrismicRichText field={slice.primary.description} />
+      </div>
     </div>
-  </section>
+    </div>
+  </div>
 )
 
 export default NouhaHeading
