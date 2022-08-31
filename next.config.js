d4594c1af3f74bc9628ab0112f810beed40cde81
Dmsooja <dev@sooja.fr>
2022-08-31 10:23:21 +0200

    Revert "Add a button to the heading"
    This reverts commit 88815460205155576118fd052c9e7a834a57b274.

--- next.config.js
+++ next.config.js
@@ -6,45 +6,45 @@
 
 // module.exports = nextConfig
 
-// const prismic = require("@prismicio/client");
+const prismic = require("@prismicio/client");
 
-// const sm = require("./sm.json");
+const sm = require("./sm.json");
 
-// const nextConfig = async () => {
-//   const client = prismic.createClient(sm.apiEndpoint);
-
-//   const repository = await client.getRepository();
-//   const locales = repository.languages.map((lang) => lang.id);
-
-//   return {
-//     reactStrictMode: true,
-//     i18n: {
-//       locales,
-//       defaultLocale: locales[0],
-// },
-//   };
-// };
-
-// module.exports = nextConfig;
-
-
-/**
- * @returns {import('next').NextConfig}
- */
-module.exports = async () => {
+const nextConfig = async () => {
   const client = prismic.createClient(sm.apiEndpoint);
 
   const repository = await client.getRepository();
   const locales = repository.languages.map((lang) => lang.id);
 
   return {
+    reactStrictMode: true,
     i18n: {
       locales,
       defaultLocale: locales[0],
-    },
-    images: {
-      loader: "imgix",
-      path: "",
-    },
+},
   };
 };
+
+module.exports = nextConfig;
+
+
+// /**
+//  * @returns {import('next').NextConfig}
+//  */
+// module.exports = async () => {
+//   const client = prismic.createClient(sm.apiEndpoint);
+
+//   const repository = await client.getRepository();
+//   const locales = repository.languages.map((lang) => lang.id);
+
+//   return {
+//     i18n: {
+//       locales,
+//       defaultLocale: locales[0],
+//     },
+//     images: {
+//       loader: "imgix",
+//       path: "",
+//     },
+//   };
+// };
