0d45bac16ef1f7f22b74f85d0f01787707051f08
Dmsooja <dev@sooja.fr>
2022-08-31 14:00:03 +0200

    Change border

--- slices/NouhaHeading/index.js
+++ slices/NouhaHeading/index.js
@@ -4,13 +4,14 @@ import { PrismicRichText, PrismicLink } from '@prismicio/react';
 
 
 const NouhaHeading = ({ slice }) => (
-  <div className="my-4 sm:flex sm:items-center sm:justify-between max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <div className="pb-5 border-b border-gray-200">    <div className='text-lg leading-6 font-medium text-gray-900'>
-      <PrismicRichText field={slice.primary.title} />
-      <div className="mt-2 max-w-4xl text-sm text-gray-500">
-        <PrismicRichText field={slice.primary.description} />
+  <div className="pb-5 border-b border-gray-200 my-4 sm:flex sm:items-center sm:justify-between max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div>
+      <div className='text-lg leading-6 font-medium text-gray-900'>
+        <PrismicRichText field={slice.primary.title} />
+        <div className="mt-2 max-w-4xl text-sm text-gray-500">
+          <PrismicRichText field={slice.primary.description} />
+        </div>
       </div>
-    </div>
     </div>
   </div>
 )
