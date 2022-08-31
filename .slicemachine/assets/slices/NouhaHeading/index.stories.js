88815460205155576118fd052c9e7a834a57b274
Dmsooja <dev@sooja.fr>
2022-08-31 10:21:14 +0200

    Add a button to the heading

--- .slicemachine/assets/slices/NouhaHeading/index.stories.js
+++ .slicemachine/assets/slices/NouhaHeading/index.stories.js
@@ -5,5 +5,8 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading3","text":"Goose","spans":[]}],"description":[{"type":"paragraph","text":"Aute pariatur sint pariatur fugiat eiusmod fugiat velit exercitation do occaecat consequat aliqua. Incididunt velit ullamco velit consectetur exercitation nulla minim.","spans":[]}]},"slice_type":"nouha_heading","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading3","text":"Broken","spans":[]}],"description":[{"type":"paragraph","text":"Elit quis proident dolor occaecat eu irure non fugiat magna consequat magna velit reprehenderit. Enim eiusmod reprehenderit est deserunt cupidatat veniam eiusmod. Aliqua minim veniam deserunt.","spans":[]}]},"slice_type":"nouha_heading","id":"_Default"}} />
 _Default.storyName = ''
+
+export const _WithCta = () => <MyComponent slice={{"variation":"withCta","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading3","text":"Managed","spans":[]}],"description":[{"type":"paragraph","text":"Ullamco ut officia aliquip consectetur nostrud consectetur magna. Aute mollit ex est cupidatat id dolor laborum ex labore voluptate reprehenderit velit.","spans":[]}],"cta_url":{"link_type":"Web","url":"http://google.com"},"cta_label":"half"},"slice_type":"nouha_heading","id":"_WithCta"}} />
+_WithCta.storyName = ''
--- slices/NouhaHeading/index.js
+++ slices/NouhaHeading/index.js
@@ -4,14 +4,30 @@ import { PrismicRichText, PrismicLink } from '@prismicio/react';
 
 
 const NouhaHeading = ({ slice }) => (
-  <div className="my-4 sm:flex sm:items-center sm:justify-between max-w-7xl mx-auto sm:px-6 lg:px-8">
-    <div className="pb-5 border-b border-gray-200">    <div className='text-lg leading-6 font-medium text-gray-900'>
-      <PrismicRichText field={slice.primary.title} />
-      <div className="mt-2 max-w-4xl text-sm text-gray-500">
-        <PrismicRichText field={slice.primary.description} />
+  <div className="my-4 border-b sm:flex sm:items-center sm:justify-between max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div className="pb-5 border-gray-200">
+      <div className='text-lg leading-6 font-medium text-gray-900'>
+        <PrismicRichText field={slice.primary.title} />
+        <div className="mt-2 max-w-4xl text-sm text-gray-500">
+          <PrismicRichText field={slice.primary.description} />
+        </div>
       </div>
     </div>
-    </div>
+    {slice.variation === "withCta" ?
+
+      <div className="mt-3 sm:mt-0 sm:ml-4">
+        <PrismicLink field={slice.primary.cta_url}>
+          <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            {slice.primary.cta_label}
+          </button>
+
+        </PrismicLink>
+      </div>
+      : null
+    }
   </div>
 )
 
