5101d77181a2a6e50fa6a982f853fdb8ea2142b7
Dmsooja <dev@sooja.fr>
2022-09-07 17:35:24 +0200

    Revert "Create Dianka heading and add to homepage and blog article page"
    This reverts commit 6e5661ecc058eb85b57eb663808785266d045fb3.

--- .slicemachine/assets/slices/DiankaDemoHeading/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/DiankaDemoHeading';
-
-export default {
-  title: 'slices/DiankaDemoHeading'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading3","text":"Accurate","spans":[]}]},"slice_type":"dianka_demo_heading","id":"_Default"}} />
-_Default.storyName = ''
