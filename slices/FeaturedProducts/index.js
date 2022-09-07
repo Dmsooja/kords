ce5c390d95af9a3e9d33b4aaf7a571c26fa7e889
Dmsooja <dev@sooja.fr>
2022-09-07 14:57:16 +0200

    Create an integration field for products

--- slices/FeaturedProducts/index.js
+++ slices/FeaturedProducts/index.js
@@ -2,10 +2,41 @@ import React from 'react';
 import { PrismicRichText, PrismicLink } from '@prismicio/react';
 
 
-
-const FeaturedProducts = ({ slice }) => (
-  <section>
-  </section>
-)
+const FeaturedProducts = ({ slice }) => {
+  return (
+    <section>
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+          <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {slice?.items?.map((item, idx) => (
+              <div key={idx} className="group relative">
+                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                  <img
+                    src={item.product.imageSrc}
+                    alt={item.product.imageAlt}
+                    className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                  />
+                </div>
+                <div className="mt-4 flex justify-between">
+                  <div>
+                    <h3 className="text-sm text-gray-700">
+                      <a href="#">
+                        <span aria-hidden="true" className="absolute inset-0" />
+                        {item.product.name}
+                      </a>
+                    </h3>
+                    {/* <p className="mt-1 text-sm text-gray-500">{item.product.color}</p> */}
+                  </div>
+                  <p className="text-sm font-medium text-gray-900">{item.product.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
 
 export default FeaturedProducts
