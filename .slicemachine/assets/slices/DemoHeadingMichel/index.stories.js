ab8ae3ceaa8d0f3bbae19e9dfeadab00ad4b281b
Dmsooja <dev@sooja.fr>
2022-09-07 15:17:40 +0200

    Create Michel Heading slice and add to homepage and blog article CT

---
+++ .slicemachine/assets/slices/DemoHeadingMichel/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/DemoHeadingMichel';
+
+export default {
+  title: 'slices/DemoHeadingMichel'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Bridge","spans":[]}]},"slice_type":"demo_heading_michel","id":"_Default"}} />
+_Default.storyName = ''
