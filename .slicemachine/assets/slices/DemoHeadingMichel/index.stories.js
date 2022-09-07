62863ff60214d3d6602fdf403686a6f583bf498d
Dmsooja <dev@sooja.fr>
2022-09-07 17:36:46 +0200

    Revert "Create Michel Heading slice and add to homepage and blog article CT"
    This reverts commit ab8ae3ceaa8d0f3bbae19e9dfeadab00ad4b281b.

--- .slicemachine/assets/slices/DemoHeadingMichel/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/DemoHeadingMichel';
-
-export default {
-  title: 'slices/DemoHeadingMichel'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Bridge","spans":[]}]},"slice_type":"demo_heading_michel","id":"_Default"}} />
-_Default.storyName = ''
