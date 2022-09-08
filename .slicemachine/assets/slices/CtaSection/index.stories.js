fcfee97674acad8f27206ea6bef5749b0ac06092
Dmsooja <dev@sooja.fr>
2022-09-08 16:45:03 +0200

    Add CTA example for demos

---
+++ .slicemachine/assets/slices/CtaSection/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/CtaSection';
+
+export default {
+  title: 'slices/CtaSection'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{},"slice_type":"cta_section","id":"_Default"}} />
+_Default.storyName = ''
