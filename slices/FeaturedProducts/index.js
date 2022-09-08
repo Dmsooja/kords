8dacb5b9a363ef4087eda7d1720448e735511c34
Dmsooja <dev@sooja.fr>
2022-09-08 16:04:41 +0200

    fix image aspect ratio

--- slices/FeaturedProducts/index.js
+++ slices/FeaturedProducts/index.js
@@ -7,11 +7,11 @@ const FeaturedProducts = ({ slice }) => {
     <section>
       <div className="bg-white">
         <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+          <h2 className="text-2xl font-bold tracking-tight text-gray-900">Most popular gear</h2>
           <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
             {slice?.items?.map((item, idx) => (
               <div key={idx} className="group relative">
-                <div className="min-h-80 aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75 lg:aspect-none lg:h-80">
+                <div className="aspect-square w-full overflow-hidden rounded-md bg-gray-200 group-hover:opacity-75">
                   <img
                     src={item.product.imageSrc}
                     alt={item.product.imageAlt}
