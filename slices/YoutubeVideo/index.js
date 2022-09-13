2049cb87eefba20a3737e85624d9fa2d63d41e17
Dmsooja <dev@sooja.fr>
2022-09-13 09:31:08 +0200

    Style Youtube embed slice

--- slices/YoutubeVideo/index.js
+++ slices/YoutubeVideo/index.js
@@ -4,12 +4,19 @@ import { PrismicRichText, PrismicLink } from '@prismicio/react';
 
 
 const YoutubeVideo = ({ slice }) => (
-  <section>
-    <div>
-      <PrismicRichText field={slice.primary.title} />
-      <div dangerouslySetInnerHTML={{ __html: slice.primary.youtube_video_link.html }} />
-      <PrismicRichText field={slice.primary.caption} />
+  <section className='container mx-auto my-6 max-w-7xl px-4 sm:px-6 lg:px-8'>
+    <div className="mx-auto max-w-3xl grid justify-items-center">
+      <div className='mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4'>
+        <PrismicRichText field={slice.primary.title} />
+      </div>
+      <div
+        className='w-300-px'
+        dangerouslySetInnerHTML={{ __html: slice.primary.youtube_video_link.html }} />
+      <div className="text-center tracking-tight text-darkgreen">
+        <PrismicRichText field={slice.primary.caption} />
+      </div>
     </div>
+
   </section>
 )
 
