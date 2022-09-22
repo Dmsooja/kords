5e0e454a75a2d751d976812a6eadfadbae7400c9
Dmsooja <dev@sooja.fr>
2022-09-22 11:30:03 +0200

    Style text slice

--- slices/Text/index.js
+++ slices/Text/index.js
@@ -3,9 +3,12 @@ import * as prismicH from '@prismicio/helpers';
 import { PrismicRichText, PrismicLink } from '@prismicio/react';
 
 const Text = ({ slice }) => (
-  <div>
-    <div >
-      <PrismicRichText field={slice.primary.text} />
+  <div className="relative overflow-hidden bg-white container mx-auto sm:px-6 lg:px-8">
+    <div className='prose prose-lg prose-indigo mx-auto mt-6 text-gray-500'>
+      {/* <div className='mt-8 text-xl leading-8 text-gray-500'> */}
+      <div className="mt-8 text-xl leading-8 text-gray-500 max-w-7xl">
+        <PrismicRichText field={slice.primary.text} />
+      </div>
     </div>
   </div>
 )
