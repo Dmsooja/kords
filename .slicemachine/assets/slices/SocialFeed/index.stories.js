47516032cf0dec249a2c2a35f6603699e48cba9c
Dmsooja <dev@sooja.fr>
2022-10-05 12:02:24 +0200

    update social feed ui

--- .slicemachine/assets/slices/SocialFeed/index.stories.js
+++ .slicemachine/assets/slices/SocialFeed/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading2","text":"Accept","spans":[]}],"social_link_label":"eventually","social_link_url":{"link_type":"Web","url":"https://slicemachine.dev"},"number_of_posts":81},"slice_type":"social_feed","id":"_Default"}} />
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading2","text":"Health","spans":[]}],"social_link_label":"agree","social_link_url":{"link_type":"Web","url":"http://twitter.com"},"number_of_posts":20},"slice_type":"social_feed","id":"_Default"}} />
 _Default.storyName = ''
--- slices/SocialFeed/index.js
+++ slices/SocialFeed/index.js
@@ -6,9 +6,11 @@ import axios from 'axios';
 const SocialFeed = ({ slice }) => {
 
   const [images, setImages] = useState([])
+  
+  const postCount = slice.primary.number_of_posts;
 
   const fetchImages = () => {
-    const flickrApi = 'https://www.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=f929272a6239bdb54c3d66101055135c&group_id=85397716@N00&sort=relevance&per_page=20&format=json&nojsoncallback=1';
+    const flickrApi = `https://www.flickr.com/services/rest/?method=flickr.groups.pools.getPhotos&api_key=f929272a6239bdb54c3d66101055135c&group_id=85397716@N00&sort=relevance&per_page=${postCount}&format=json&nojsoncallback=1`;
 
     const api = flickrApi;
 
@@ -21,7 +23,11 @@ const SocialFeed = ({ slice }) => {
       });
   }
 
-  const postCount = slice.primary.number_of_posts;
+  useEffect(() => {
+    fetchImages()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
 
   return (
     <section>
@@ -39,37 +45,30 @@ const SocialFeed = ({ slice }) => {
           <div className="mt-4 flow-root">
             <div className="-my-2">
               <div className="box-content py-2 relative h-80 overflow-x-auto flex">
-                <div className="absolute min-w-screen-xl px-4 flex space-x-8 sm:px-6 lg:px-8 xl:relative xl:px-0 xl:space-x-0 xl:grid xl:grid-cols-5 xl:gap-x-8">
-                  {/* <div className="flex-none py-6 px-3 first:pl-6 last:pr-6"> */}
+                <div className="absolute min-w-screen-xl px-4 flex space-x-8 sm:px-6 lg:px-8">
                   {/* {slice?.items?.map((item, idx) => ( */}
-                  {images?.map(({ server, id, secret }, idx) => (
-                    <div
+                  {images?.map(({ server, id, secret, owner, title }, idx) => (
+                    <a
+                      href={`https://www.flickr.com/photos/${owner}/${id}`}
                       className="relative w-80 h-80 rounded-lg p-6 flex flex-col overflow-hidden hover:opacity-75"
-                      // className="flex flex-col items-center justify-center gap-3"
                       key={idx}
                     >
                       <span aria-hidden="true" className="absolute inset-0">
                         <img
                           src={`https://live.staticflickr.com/${server}/${id}_${secret}.jpg`}
-                          alt=""
+                          alt={title}
                           className="w-full h-full object-center object-cover" />
                       </span>
                       <span
                         aria-hidden="true"
                         className="absolute inset-x-0 bottom-0 h-2/3 bg-gradient-to-t from-gray-800 opacity-50"
                       />
-                      <span className="relative mt-auto text-center text-xl font-bold text-white">Card label</span>
-                    </div>
+                      <span className="relative mt-auto text-center text-xl font-bold text-white">{title}</span>
+                    </a>
                   ))}
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="mt-6 px-4 sm:hidden">
-            {/* <PrismicLink field={slice.primary.section_link} className="block text-sm font-semibold text-indigo-600 hover:text-indigo-500">
-            <PrismicRichText field={slice.primary.sectionLinkLabel} /><span aria-hidden="true"> &rarr;</span>
-          </PrismicLink> */}
           </div>
         </div>
       </div>
